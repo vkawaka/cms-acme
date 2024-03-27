ab4975a50292fb4bd8be72345ab9c7bffd5a09c0
unknown <nathaliavkawakami@gmail.com>
2024-03-27 19:56:07 -0300

    object Promise vc me paga

--- main.js
+++ main.js
@@ -2,18 +2,38 @@
 
 import { getFilmes, getFilme, postFilme, deleteFilme } from "./filmes.js"
 
-const createSpace = async(id) =>{
-    const container = document.getElementById('cont')
-
-    const filme = document.createElement('div')
+const createSpace = async(filme) =>{
+    const filmeE = document.createElement('div')
     const nome = document.createElement('p')
+    nome.textContent = filme.nome
+    console.log(filme.nome);
     const editBtn = document.createElement('button')
     const deleteBtn = document.createElement('button')
     const editI = document.createElement('img')
     editI.src = './edit.png'
-    editBtn.id = `edit${id}`
+    // editBtn.id = `edit${id}`
     const deleteI = document.createElement('img')
     deleteI.src = './trash.png'
-    deleteBtn.id = `trash${id}`
+    // deleteBtn.id = `trash${id}`
+    deleteBtn.append(deleteI)
+    editBtn.append(editI)
+
+
+    filmeE.append(nome)
+
+    return filmeE
     
 }
+
+async function preencherTela(){
+    const container = document.getElementById('cont')
+
+    const filmes = await getFilmes()
+    
+    filmes.forEach(element => {
+        const card = createSpace(element)
+        container.append(card)
+    })
+}
+
+preencherTela()
